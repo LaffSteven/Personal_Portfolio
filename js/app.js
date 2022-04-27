8a653d2c443385de191f4a948de4e8baacf63371
LaffSteven <steve.laff@gmail.com>
2022-04-27 12:55:50 -0400

    basic projects page completed

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 
 
 
+const showModal = () => {
+  $('.contact-modal').show('slow');
+}
 
+const hideModal = () => {
+  $('.contact-modal').hide();
+}
+
+const goToHome = (url) => {
+  $(location).attr('href', 'index.html');
+}
+const goToProjects = (url) => {
+  $(location).attr('href', 'projects.html');
+}
+const goToResume = (url) => {
+  $(location).attr('href', 'resume.html');
+}
+const goToAbout = (url) => {
+  $(location).attr('href', 'about.html');
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 $(() => {
+
+  $('#contact').on('click', showModal);
+  $('#contact-conatiner').on('click', showModal);
+  $('#close-button').on('click', hideModal);
 
 
 })
